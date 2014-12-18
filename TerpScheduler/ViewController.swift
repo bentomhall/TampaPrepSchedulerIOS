2edda9d9297f2e3a4dd22d73eb96710bb3b9beb2
Ben Hall <bhall@tampaprep.org>
2014-12-18 10:46:15 -0500

    Initial Commit

---
+++ TerpScheduler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TerpScheduler
+//
+//  Created by Ben Hall on 12/18/14.
+//  Copyright (c) 2014 Tampa Preparatory School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
